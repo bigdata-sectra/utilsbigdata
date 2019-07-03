81a1fb0e635987583784b8b256c6aa6e96d2ac36
leocamus <leocamusc@gmail.com>
2019-07-03 16:51:26 -0400

    this is a first structure

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='utilsbigdata',
-      version='0.2',
+      version='0.3',
       description='Utils for Big Data - MTT',
       url='git@github.com:bigdata-sectra/utilsbigdata.git',
       author='Big Data - MTT',
       author_email='bigdata@sectra.gob.cl',
       license='MIT',
-      packages=['utilsbigdata'],
+      packages=find_packages(),
       zip_safe=False)
